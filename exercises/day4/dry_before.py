c9ecb9b8551c20f000bc6b05fb6ce78ceb12c3ab
yash-solanki09442 <yashsolanki09442@gmail.com>
2026-02-18 05:37:42 +0000

    Quick fix

--- exercises/day4/dry_before.py
+++ exercises/day4/dry_before.py
@@ -1,10 +1,10 @@
 def send_welcome_email(user_email: str) -> None:
-    print(f"Connecting to SMTP server...")
-    print(f"Authenticating...")
+    print("Connecting to SMTP server...")
+    print("Authenticating...")
     print(f"Sending email to {user_email}: Welcome!")
 
 
 def send_password_reset_email(user_email: str) -> None:
-    print(f"Connecting to SMTP server...")
-    print(f"Authenticating...")
+    print("Connecting to SMTP server...")
+    print("Authenticating...")
     print(f"Sending email to {user_email}: Reset your password.")
