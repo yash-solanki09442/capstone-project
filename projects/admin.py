a599d87241e81e5705200c588b267f9a5632b135
yash-solanki09442 <yashsolanki09442@gmail.com>
2026-02-20 11:05:14 +0000

    add django admin and raw SQL script using psycopg2

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Project
 
-# Register your models here.
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ("title", "slug", "owner", "created_at")
+    search_fields = ("title", "slug", "owner__username")
+    list_filter = ("created_at",)
+    list_select_related = ("owner",)
