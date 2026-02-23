f4192147910ae89d63ad357f4f3d932fc66777de
yash-solanki09442 <yashsolanki09442@gmail.com>
2026-02-23 07:52:45 +0000

    add serialzers for Task, Comment & Project

---
+++ api/serializers.py
@@ -0,0 +1,5 @@
+from projects.serializers import ProjectSerializer
+from tasks.serializers import TaskSerializer
+from comments.serializers import CommentSerializer
+
+__all__ = ["ProjectSerializer", "TaskSerializer", "CommentSerializer"]
---
+++ comments/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from comments.models import Comment
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = "__all__"
+        read_only_fields = ["id", "created_at"]
