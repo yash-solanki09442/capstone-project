6e416a63bbf3bfcd1ebe2d38b42986ae66aeae3c
yash-solanki09442 <yashsolanki09442@gmail.com>
2026-02-25 07:11:59 +0000

    finalize Milestone 2

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,13 @@
 from django.urls import include, path
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from . import views
 
 urlpatterns = [
+    # OpenAPI schema + Swagger UI
+    path("schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
     # JWT auth endpoints
     path("auth/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("auth/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
